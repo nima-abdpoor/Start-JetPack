b3520a75903e1789acf52a007db274f9c439b33b
nima abdpoor <nimaabdi328@yahoo.com>
2020-11-22 16:21:40 +0330

    composable function and woring with text

--- app/src/main/java/com/nima/myapplication/MainActivity.kt
+++ app/src/main/java/com/nima/myapplication/MainActivity.kt
@@ -3,15 +3,28 @@ package com.nima.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.compose.foundation.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
-
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.ui.tooling.preview.Preview
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World")
+            Greeting()
         }
+    }
+    @Composable
+    @Preview
+    private fun Greeting() {
+        Text(
+                text = "Hello, World!",
+                style = TextStyle(color = Color.Blue) ,
+                fontSize = TextUnit(200L)
+                )
     }
 }
